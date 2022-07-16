f2ab823f4edd4194d2865b2c7ddb74b0141e9d75
Lacassagne <godzilla77000@hotmail.fr>
2022-07-16 03:05:03 +0200

    Unity2

---
+++ Assets/test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void testff(InputAction.CallbackContext context) 
+    {
+        print("test");
+    }
+}
